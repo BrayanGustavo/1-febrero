2dabcbcb0c0f5ae0d3526cd6012c56d9748770eb
BrayanGustavo <47338966+BrayanGustavo@users.noreply.github.com>
2019-02-17 00:07:27 -0600

    Add files via upload
    Programación Arduino: Tiempo Encendido y Tiempo Apagado 3 Leds.

---
+++ 3_leds.ino
@@ -0,0 +1,26 @@
+int ledR = 13;
+int ledG = 12;
+int ledB = 11;
+int enc = 50;
+int apa = 10;
+
+void setup() {
+pinMode(ledR,OUTPUT);
+pinMode(ledG,OUTPUT);
+pinMode(ledB,OUTPUT);
+}
+
+void loop() {
+digitalWrite(ledR,HIGH);
+delay(enc);
+digitalWrite(ledR,LOW);
+delay(apa);
+digitalWrite(ledG,HIGH);
+delay(enc);
+digitalWrite(ledG,LOW);
+delay(apa);
+digitalWrite(ledB,HIGH);
+delay(enc);
+digitalWrite(ledB,LOW);
+delay(apa);
+}
